102f29ec05775b01ac0409efa9ce47226bde976a
fifthcalibr <138707838+fifthcalibr@users.noreply.github.com>
2023-08-08 10:22:53 -0700

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Header from "./Components/Header/Header"
 import "./App.scss"
+import Home from "./Components/Home/Home"
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+
+      <main>
+        <div className="page" id="page">
+          <Home />
+        </div>
+      </main>
+
     </div>
   )
 }
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Home.scss"
+
+const Home = () => {
+  return <div className='home'>
+    <div className="details">
+        <span className="intro">Hello, I am</span>
+
+        <div className="name">
+            <div>
+                <h1 className='txt'>GR <br />AC</h1>
+            </div>
+            <h1 className='txt lg'>E</h1>
+            <div className="stack">
+                FULL-STACK <b>DEVELOPER</b>
+            </div>
+        </div>
+    </div>
+
+    <div className="profile-img"></div>
+  </div>
+}
+
+export default Home
