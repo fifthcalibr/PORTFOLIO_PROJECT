1d59a252e7a4fa4ed1527c0c30d6080bc27c50a6
fifthcalibr <138707838+fifthcalibr@users.noreply.github.com>
2023-08-11 12:57:55 -0700

    Contact is done. Minor Design corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,116 @@
-import Header from "./Components/Header/Header"
-import "./App.scss"
-import Home from "./Pages/Home/Home"
-import About from "./Pages/About/About"
-import Social from "./Components/Social/Social"
-import Projects from "./Pages/Projects/Projects"
+import Header from "./Components/Header/Header";
+import "./App.scss";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Social from "./Components/Social/Social";
+import Projects from "./Pages/Projects/Projects";
+import Contact from "./Pages/Contact/Contact";
 
-import github from "./assets/github-mark.png"
-import linkedin from "./assets/linkedin.png"
-import discord from "./assets/discord.png"
-import email from "./assets/email.png"
-import messenger from "./assets/messenger.png"
-import Contact from "./Pages/Contact/Contact"
+import github from "./assets/github-mark.png";
+import linkedin from "./assets/linkedin.png";
+import discord from "./assets/discord.png";
+import email from "./assets/email.png";
+import messenger from "./assets/messenger.png";
+import { useEffect } from "react";
 
 function App() {
+    useEffect(() => {
+        const navLinks = document.querySelectorAll("nav ul li");
+        const indicator = document.querySelector("nav .indicator");
+        const profile = document.querySelector(".profile");
 
-  return (
-    <div className="App">
-      <Header />
+        const pages = document.querySelectorAll(".page");
 
-      <main>
-        <div className="page" id="home">
-          <Home />
+        const handleScroll = () => {
+            let homeBottom = pages[0].getBoundingClientRect().bottom;
+            let homeHeight = pages[0].offsetHeight;
+
+            if(homeBottom < homeHeight / 2) {
+              profile.classList.add("active");
+            } else {
+              profile.classList.remove("active");
+            }
+
+            for (let i = 0; i < pages.length; i++) {
+              let currentPage = pages[i];
+
+              let pageTop = currentPage.getBoundingClientRect().top;
+
+              if(pageTop < 300) {
+
+                navLinks.forEach(link => {
+                  link.classList.remove("active");
+                })
+
+                navLinks[i].classList.add("active");
+
+                let width = navLinks[i].offsetWidth,
+                    left = navLinks[i].offsetLeft;
+
+                indicator.style.width = `${width + 28}px`;
+                indicator.style.left = left + "px";
+              }
+            }
+        };
+
+        handleScroll()
+        window.addEventListener("scroll", handleScroll)
+    }, []);
+    return (
+        <div className="App">
+            <Header />
+
+            <main>
+                <div className="page" id="home">
+                    <Home />
+                </div>
+
+                <div className="page" id="about">
+                    <About />
+                </div>
+
+                <div className="socials-wrapper">
+                    <h2 className="quick-links">Quick Links</h2>
+
+                    <div className="socials">
+                        <Social
+                            logo={github}
+                            title="GitHub"
+                            link="https://github.com/fifthcalibr"
+                        />
+                        <Social
+                            logo={linkedin}
+                            title="LinkedIn"
+                            link="http://www.linkedin.com/in/rinat-zabirov-316740226"
+                        />
+                        <Social
+                            logo={email}
+                            title="Email"
+                            link="mailto:rinatkaka3@gmail.com"
+                        />
+                        <Social
+                            logo={messenger}
+                            title="Messenger"
+                            href="vk.com"
+                        />
+                        <Social
+                            logo={discord}
+                            title="Discord"
+                            link="http://discordapp.com/users/fifthcalibr#9992"
+                        />
+                    </div>
+                </div>
+
+                <div className="page" id="projects">
+                    <Projects />
+                </div>
+
+                <div className="page" id="contact">
+                    <Contact />
+                </div>
+            </main>
         </div>
-
-        <div className="page" id="about">
-          <About />
-        </div>
-        
-        <div className="socials-wrapper">
-          <h2 className="quick-links">Quick Links</h2>
-
-          <div className="socials">
-            <Social logo = {github} title = "GitHub" link = "https://github.com/fifthcalibr"/>
-            <Social logo = {linkedin} title = "LinkedIn" link = "http://www.linkedin.com/in/rinat-zabirov-316740226" />
-            <Social logo = {email} title = "Email" link = "mailto:rinatkaka3@gmail.com"/>
-            <Social logo = {messenger} title = "Messenger" href ="vk.com"/>
-            <Social logo = {discord} title = "Discord" link = "http://discordapp.com/users/fifthcalibr#9992" />
-          </div>
-        </div>
-
-        <div className="page" id="projects">
-          <Projects />
-        </div>
-
-        <div className="page" id="contact">
-          <Contact />
-        </div>
-      </main>
-
-    </div>
-  )
+    );
 }
 
-export default App
+export default App;
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import email from "../../assets/email.png"
+import messenger from "../../assets/messenger.png"
+import "./Contact.scss"
+import Social from '../../Components/Social/Social.jsx'
+
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <h1>Get in Touch</h1>
+      <span>sdsdsd</span>
+      <div className="socials">
+        <Social logo = {email} title = "Email" link = "mailto:rinatkaka3@gmail.com"/>
+        <Social logo = {messenger} title = "LinkedIn" link = "mailto:rinatkaka3@gmail.com"/>
+      </div>
+
+      <footer>
+        <span>I am footer</span>
+      </footer>
+    </div>
   )
 }
 
